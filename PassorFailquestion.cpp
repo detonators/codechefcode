bf00c382bc4cd0a5e0145403ac06d82b1274c358
Shubham Singh <87699417+detonators@users.noreply.github.com>
2024-10-20 23:35:36 +0530

    Update PassorFailquestion.cpp

--- PassorFailquestion.cpp
+++ PassorFailquestion.cpp
@@ -3,11 +3,13 @@
 using namespace std;
 typedef long long ll;
 int32_t main(){
+
     int t;
     cin>>t;
     while(t--){
         int n , x , p;
          cin>>n>>x>>p;
+
          if ((x*3-(n-x)>=p)) {
      cout<< "PASS"<<"\n";
     }
